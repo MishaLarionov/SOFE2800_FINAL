093f9c970a3ed185cb8e030b9369624d23d2be6d
MishaLarionov <MishaLarionov@users.noreply.github.com>
2020-12-04 13:35:59 -0500

    Make offer posts to self

--- makeOffer.php
+++ makeOffer.php
@@ -12,11 +12,43 @@ if (!$conn) {
 session_start();
 //echo "session id is: ". $_SESSION['sessionID']."<br>";
 
+$error = "";
+
 // Get post request variables
 $listingid = $_POST["listingid"];
 //echo $listingid;
 
-$query="SELECT * FROM listing WHERE id= '$listingid';";
+// If offerdesc is set this is a request to create an offer
+if (isset($_POST["offerdesc"])) {
+
+    // Get post request variables
+    $fromUserid = $_POST["fromUserid"];
+    $toUserid = $_POST["toUserid"];
+    $listingid = $_POST["listingid"];
+    $contact = $_POST["contact"];
+    $offerdesc = $_POST["offerdesc"];
+
+    // Make a sql query to see if the offer was already made
+    $sql = "SELECT id FROM offer WHERE fromUserid = '$fromUserid' AND listingid = '$listingid';";
+    if (mysqli_query($conn, $sql)->num_rows > 0) {
+        $offerExists = true;
+    } else {
+        $offerExists = false;
+    }
+    //if the offer was already made don't make another one
+    if ($offerExists == true) {
+        // Redirect back to form with error
+        $error = "You have already made an offer for this listing!";
+    } else {
+        $sql = "INSERT INTO offer (fromUserid, toUserid, listingid,contact,offerdesc) VALUES ('$fromUserid','$toUserid','$listingid','$contact','$offerdesc');";
+        mysqli_query($conn, $sql);
+        // Redirect to sent offers
+        header("Location: viewSentOffers.php");
+        die();
+    }
+}
+
+$query = "SELECT * FROM listing WHERE id= '$listingid';";
 $qresult = $conn->query($query);
 
 // Obtains the data contained for the matching table row.
@@ -27,13 +59,12 @@ $image = $row['image'];
 $userid = $row['userid'];
 
 
-$query="SELECT username FROM user WHERE id= '$userid';";
-$qresult = mysqli_query($conn,$query);
+$query = "SELECT username FROM user WHERE id= '$userid';";
+$qresult = mysqli_query($conn, $query);
 
 // Obtains the data contained for the matching table row using mysqli.
 $row = mysqli_fetch_array($qresult, MYSQLI_ASSOC);
 $postingUsername = $row['username'];
-
 
 ?>
 
@@ -46,7 +77,7 @@ $postingUsername = $row['username'];
 <?php include('header.php')?>
 <div class="pageContent">
     <h1>Make an offer on <?php echo $postingUsername ?>'s <?php echo $listingTitle ?></h1>
-    <form action="checkOffer.php" method="post">
+    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
         <input type="text" name="fromUserid" value="<?php echo $_SESSION['sessionID'] ?>" style="display: none">
         <input type="text" name="toUserid" value="<?php echo $userid ?>" style="display: none">
         <input type="text" name="listingid" value="<?php echo $listingid ?>"style="display: none">
@@ -57,6 +88,7 @@ $postingUsername = $row['username'];
         <label for="offerdesc">Describe your offer</label>
         <textarea name="offerdesc" placeholder="Enter a description of your offer here." id="" cols="30" rows="10"></textarea>
         <input type="submit">
+        <p class="error"><?php echo $error ?></p>
 </div>
 
 
