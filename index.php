a866c15eb20361257550b4aa04d9afe0e6693fbc
MishaLarionov <MishaLarionov@users.noreply.github.com>
2020-12-03 19:47:39 -0500

    Index redirects

--- index.php
+++ index.php
@@ -1,20 +1,5 @@
 <?php
 
-// Todo test this lol
-//include_once("database.php");
-session_start();
-$_SESSION['sessionID']=null;
-include_once("login.php")
-
-?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <title>Final Project</title>
-    <!-- Todo include javascript and such, probably from separate PHP file -->
-</head>
-<body>
-<h1><?php if($_SESSION['sessionID']!=null){echo "Session Id: ".$_SESSION['sessionID'];}?></h1>
-</body>
-</html>
+// Redirect to login (or whichever page we choose to be the main page)
+header("Location: login.php");
+die();
