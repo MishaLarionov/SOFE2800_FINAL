7b1b1948d47a71b07e2dd931fedcf9df0cd5a16b
MishaLarionov <MishaLarionov@users.noreply.github.com>
2020-12-03 19:42:50 -0500

    Login page post to self

--- login.php
+++ login.php
@@ -1,5 +1,72 @@
 <?php
 
+$error = "";
+
+// Check if this is a post request
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Include and call function to connect to db
+    include_once 'components/dbConnection.php';
+    $conn = getConnection();
+
+    // Check connection
+    if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+    session_start();
+
+    // Get post request variables
+    //$email = $_POST["email"];
+    $password = $_POST["password"];
+    $mode = $_POST["mode"];
+    //$firstName = $_POST["firstName"];
+    //$lastName = $_POST["lastName"];
+    $username = $_POST["username"];
+
+    // Make a sql query to see if the user exists (This is useful for both login and signup)
+    $sql = "SELECT username FROM user WHERE username = '$username';";
+    if (mysqli_query($conn, $sql)->num_rows > 0) {
+        $userExists = true;
+    } else {
+        $userExists = false;
+    }
+
+    if ($mode == "login") {
+        // Make sure the user exists
+        if ($userExists) {
+
+            // Get a user matching the email/password pair
+            $sql = "SELECT username ,id FROM user WHERE username = '$username' AND passwordHash = '$password';";
+            $result = mysqli_query($conn, $sql);
+            // If nothing was returned, the password is wrong
+            if ($result->num_rows == 0) {
+                echo "Wrong password!";
+            } else {
+                while ($row = $result->fetch_assoc()) {
+                    echo "Welcome " . $row['username'] . "<br>";
+                    $_SESSION['sessionID'] = $row['id'];
+                    //echo "your User id is ".$row['id']."<br>" ."session id is the same: ".$_SESSION['sessionID'];
+                    header("Location:home.php");
+
+                }
+            }
+        } else {
+            $error = "User not found! Check your email address and try again.";
+        }
+    } else if ($mode == "signup") {
+        // Check if the user exists
+        if ($userExists) {
+            $error = "A user already exists with that username";
+        } else {
+            // Insert the new info into the database
+            $sql = "INSERT INTO user (username, passwordHash) VALUES ('$username','$password');";
+            mysqli_query($conn, $sql);
+            // Todo redirect to main page
+            $error = "Success! It worked! Yay!";
+        }
+    }
+}
+
+
 ?>
 
 <html>
@@ -17,7 +84,8 @@
     </div>
     <div id="formBody">
         <h1 class="formHeader">Login to my website</h1>
-        <form method="post" action="checkUser.php">
+        <!-- Form posts to itself -->
+        <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
             <!--
             <label for="firstName" class="signup">First Name</label>
             <input type="text" id="firstName" class="signup" name="firstName">
@@ -40,6 +108,7 @@
             <input type="text" value="login" id="mode" name="mode" hidden>
 
             <input type="submit" id="submit">
+            <p style="color: red"><?php echo $error; ?></p>
         </form>
     </div>
 </div>
